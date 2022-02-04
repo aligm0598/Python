07bdbd53738965d2b3883a241754b9a0ea1121aa
john <aligm0598@gmail.com>
2022-02-04 12:50:50 -0500

    HelloWorld.py synch with github

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,14 @@
 print('hello python world from John')
+print('hello world')
+a = 3
+b = 4
+c = a * b
+print('a * b =', c)
+
+d = a - b
+print('a - b =', d) 
+a = 2345
+b = 23434
+c = a * b
+print('a * b=', c)
+
