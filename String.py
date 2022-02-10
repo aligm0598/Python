cfc12995ce981fd5deb7d8534a5e584e93eaa9cd
john <aligm0598@gmail.com>
2022-02-10 17:29:54 -0500

    How to string

---
+++ String.py
@@ -0,0 +1,7 @@
+# Author: John 
+# Date: Feburary 10 2022
+# This program is about how to capitalize string in code
+firstname = input('What is your first name ')
+lastname = input('What is your lastname ')
+fullname = firstname.capitalize() + " " + lastname.capitalize()
+print('Hello ' + fullname)
