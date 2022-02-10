011a74e1802a9839d69b27c8b8d912c52442d44e
john <aligm0598@gmail.com>
2022-02-10 17:36:57 -0500

    To get rid of fullname

--- String.py
+++ String.py
@@ -3,5 +3,5 @@
 # This program is about how to capitalize string in code
 firstname = input('What is your first name ')
 lastname = input('What is your lastname ')
-fullname = firstname.capitalize() + " " + lastname.capitalize()
-print('Hello ' + fullname)
+#fullname = firstname.capitalize() + " " + lastname.capitalize()
+print('Hello ' + firstname.capitalize() + " " + lastname.capitalize() )
