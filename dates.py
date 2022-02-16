9750d9187b9b56b45f3841b5fb931b76ac236d0c
john <aligm0598@gmail.com>
2022-02-15 21:07:27 -0500

    learn about dates

---
+++ dates.py
@@ -0,0 +1,14 @@
+#Author: John
+#Date: feburary 15 2022
+# This program is about dates
+from datetime import datetime, timedelta
+
+today = datetime.now()
+one_day = timedelta(days=1)
+yesterday = today - one_day 
+print('Today is: ' + str(today))
+print('Yesterday was: ' + str(yesterday))
+
+birthday = input('When is your birthday (dd/mm/yyyy)? ')
+birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
+print ('Birthday: ' +str(birthday_date) +' '+ str(birthday_date.strftime('%A')))
