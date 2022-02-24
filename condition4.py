a855a01a7694ee8ce857d5f8f9d764e9b7d12237
john <aligm0598@gmail.com>
2022-02-24 17:49:07 -0500

    learned complex conditions

---
+++ condition4.py
@@ -0,0 +1,7 @@
+gpa = float(input('What is your grade point average? '))
+lowest = float(input('What is your lowest grade? '))
+
+if gpa >= .85 and lowest >= .70:
+    print('You made the honour roll ')
+else:
+    print('You did not make the honuor roll')
