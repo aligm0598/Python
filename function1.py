bc411bde74603aa44597a399512d3d5792095905
john <aligm0598@gmail.com>
2022-03-26 16:27:35 -0400

    function

---
+++ function1.py
@@ -0,0 +1,11 @@
+# requirement: ask your firstname, middle name, last name, and print out initial.
+first_name = input ('Enter your first name ')
+first_name_initial = first_name[0:1]
+
+middle_name = input ('Enter your middle name ')
+middle_name_initial = middle_name[0:1]
+
+last_name = input ('Enter your last name ')
+last_name_initial = last_name[0:1]
+
+print('Your initials are: ' + first_name_initial + middle_name_initial + last_name_initial)
