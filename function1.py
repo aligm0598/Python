f46c5d9aef70d9931f5d68523c83f4aae8c4646a
john <aligm0598@gmail.com>
2022-03-26 16:34:38 -0400

    get_initial change

--- function1.py
+++ function1.py
@@ -1,11 +1,17 @@
-# requirement: ask your firstname, middle name, last name, and print out initial.
+# requirement1: ask your firstname, middle name, last name, and print out initial.
+# requirement2: get a function call get_initial, and use it to extract initials for all the names
+def get_initial(name):
+     initial = name[0:1]
+     return initial
+
+
 first_name = input ('Enter your first name ')
-first_name_initial = first_name[0:1]
+first_name_initial = get_initial(first_name)
 
 middle_name = input ('Enter your middle name ')
-middle_name_initial = middle_name[0:1]
+middle_name_initial = get_initial(middle_name)
 
 last_name = input ('Enter your last name ')
-last_name_initial = last_name[0:1]
+last_name_initial = get_initial(last_name)
 
 print('Your initials are: ' + first_name_initial + middle_name_initial + last_name_initial)
