e9ccb263586345cf95292a09d7b512e7924eee7b
john <aligm0598@gmail.com>
2022-07-13 10:17:55 -0400

    formatting and linting

---
+++ docstrings_typehints.py
@@ -0,0 +1,17 @@
+from email import message
+
+
+def print_hello(name:str) -> str:
+    """
+    Returns a greeting 
+
+    parameter:
+        name (str): The name of the person
+
+    Returns:
+        The cool message
+    """
+
+    print('Hello, ' + name)
+
+    
